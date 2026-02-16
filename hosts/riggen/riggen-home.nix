f589837432a923d982c77aedb2cb9bfe90c86fc3
Farlot <m.waaagan@gmail.com>
2026-02-16 22:24:31 +0100

    desktop nix separation

--- hosts/riggen/riggen-home.nix
+++ hosts/riggen/riggen-home.nix
@@ -16,6 +16,7 @@
         inputs.self.homeManagerModules.yazi
         inputs.self.homeManagerModules.stylix
         inputs.self.homeManagerModules.neovim
+        inputs.self.homeManagerModules.desktop
       ];
 
       # 2. Base identity
@@ -23,75 +24,11 @@
       home.username = "${username}";
       home.homeDirectory = "/home/${username}";
 
-      home.packages = with pkgs; [
-        obsidian
-        flameshot
-        git
-        teams-for-linux
-        spotify
-        element-desktop
-        protontricks
-        mangohud
-        goverlay
-        xivlauncher
-        lutris
-        qbittorrent
-        tldr
-        itch
-        wowup-cf
-        qdirstat
-        discord
-        appimage-run
-        gocryptfs # encrypt vault
-        ouch
-        prismlauncher # Minecraft Launcher
-        flatpak # package
-        umu-launcher # game
-        obs-studio # streaming
-        ydotool # Autohotkey ish
-        rust-stakeholder # rust
-        autorandr # monitor
-        loupe # image
-        keepassxc # password
-        btop # process
-        nh # game
-        gimp # image
-        pavucontrol # audio
-        hyprshot # screenshot
-        libnotify # notification
-        kitty # terminal
-        calcurse # calendar
-        mpv # mediaplayer
-      ];      
-
       programs.zoxide = { # terminal navigation tool
         enable = true;
         enableZshIntegration = true;
       };
       programs.fzf.enable = true; # terminal navigation helper for zoxide
-
-
-      programs.brave = {
-        enable = true;
-        extensions = [
-          { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
-          { id = "oboonakemofpalcgghocfoadofidjkkk"; } # KeePassXC
-          { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # Dark Reader
-          { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # SponsorBlock
-          { id = "mmnhjecbajmgkapcinkhdnjabclcnfpg"; } # Reddit Promoted Ad Blocker
-          { id = "omoinegiohhgbikclijaniebjpkeopip"; } # Clickbait Remover Youtube
-          { id = "gebbhagfogifgggkldgodflihgfeippi"; } # Return YouTube Dislike
-          { id = "kbmfpngjjgdllneeigpgjifpgocmfgmb"; } # Reddit Enhancement Suite
-          { id = "fchmanciglollaagnijpcagpofejennb"; } # Twitch Channel Point Auto Claimer
-          { id = "ammjkodgmmoknidbanneddgankgfejfh"; } # 7TV
-          #{ id = "ajopnjidmegmdimjlfnijceegpefgped"; } # BetterTTV
-        ];
-        commandLineArgs = [
-          "--disable-features=PasswordManagerOnboarding"
-          "--disable-features=AutofillEnableAccountWalletStorage"
-        ];
-      };
-
 
       sops = {
         defaultSopsFile = ../../secrets/secrets.yaml;
@@ -120,46 +57,7 @@
         };
       };
 
-      # ADD SwayNotificationCenter
-      services.swaync = {
-        enable = true;
-        settings = {
-          # Optional: Settings to customize the look
-          positionX = "right";
-          positionY = "top";
-          layer = "overlay";
-          control-center-layer = "top";
-          layer-shell = true;
-          cssPriority = "application";
-          control-center-margin-top = 0;
-          control-center-margin-bottom = 0;
-          control-center-margin-right = 0;
-          control-center-margin-left = 0;
-          notification-2fa-action = true;
-          notification-inline-replies = false;
-          notification-icon-size = 64;
-          notification-body-image-height = 100;
-          notification-body-image-width = 200;
-        };
-        # You can also add a style.css here if you want to match your Stylix/Tokyo Night theme
-        # style = '' ... ''; 
-      };
-
-      programs.zsh = {
-        enable = true;
-        enableCompletion = true;
-        autosuggestion.enable = true;
-        syntaxHighlighting.enable = true;
-        shellAliases = {
-          v-up = "gocryptfs /mnt/spin/.vault_encrypted /mnt/spin/vault";
-          v-down = "fusermount -u /mnt/spin/vault";
-        };
-        oh-my-zsh = {
-          enable = true;
-          plugins = [ "git" "sudo" "docker" ];
-          theme = "robbyrussell"; # or "agnoster", "powerlevel10k", etc.
-        };
-      };
+      
 
       # Let Home Manager install and manage itself
       programs.home-manager.enable = true;
---
+++ modules/desktop.nix
@@ -0,0 +1,109 @@
+{ inputs, ... }:
+
+{
+  # Expose this as a home-manager module for your hosts to import
+  flake.homeManagerModules.desktop = { pkgs, config, lib, ... }: {
+    
+    home.packages = with pkgs; [
+      # --- Communication & Productivity ---
+      obsidian
+      teams-for-linux
+      element-desktop
+      discord
+      
+      # --- Media & Web ---
+      spotify
+      mpv
+      
+      # --- Utilities & Graphics ---
+      flameshot
+      hyprshot
+      loupe
+      gimp
+      pavucontrol
+      keepassxc
+      libnotify
+      qdirstat
+      
+      # --- Gaming (Consider moving to a separate games.nix later) ---
+      protontricks
+      mangohud
+      goverlay
+      xivlauncher
+      lutris
+      itch
+      wowup-cf
+      prismlauncher
+      umu-launcher
+      
+      # --- Misc Desktop ---
+      qbittorrent
+      obs-studio
+      flatpak
+      appimage-run
+      btop
+      calcuse # Calendar
+      ouch # CLI unzipperino
+      tldr
+    ];
+
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      shellAliases = {
+        v-up = "gocryptfs /mnt/spin/.vault_encrypted /mnt/spin/vault";
+        v-down = "fusermount -u /mnt/spin/vault";
+      };
+      oh-my-zsh = {
+        enable = true;
+        plugins = [ "git" "sudo" "docker" ];
+        theme = "robbyrussell"; # or "agnoster", "powerlevel10k", etc.
+      };
+    };
+
+    # --- Desktop Programs ---
+    programs.brave = {
+      enable = true;
+      extensions = [
+        { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
+        { id = "oboonakemofpalcgghocfoadofidjkkk"; } # KeePassXC
+        { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # Dark Reader
+        { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # SponsorBlock
+        { id = "mmnhjecbajmgkapcinkhdnjabclcnfpg"; } # Reddit Promoted Ad Blocker
+        { id = "omoinegiohhgbikclijaniebjpkeopip"; } # Clickbait Remover Youtube
+        { id = "gebbhagfogifgggkldgodflihgfeippi"; } # Return YouTube Dislike
+        { id = "kbmfpngjjgdllneeigpgjifpgocmfgmb"; } # Reddit Enhancement Suite
+        { id = "fchmanciglollaagnijpcagpofejennb"; } # Twitch Channel Point Auto Claimer
+        { id = "ammjkodgmmoknidbanneddgankgfejfh"; } # 7TV
+      ];
+      commandLineArgs = [
+        "--disable-features=PasswordManagerOnboarding"
+        "--disable-features=AutofillEnableAccountWalletStorage"
+      ];
+    };
+
+    # --- Desktop Services ---
+    services.swaync = {
+      enable = true;
+      settings = {
+        positionX = "right";
+        positionY = "top";
+        layer = "overlay";
+        control-center-layer = "top";
+        layer-shell = true;
+        cssPriority = "application";
+        control-center-margin-top = 0;
+        control-center-margin-bottom = 0;
+        control-center-margin-right = 0;
+        control-center-margin-left = 0;
+        notification-2fa-action = true;
+        notification-inline-replies = false;
+        notification-icon-size = 64;
+        notification-body-image-height = 100;
+        notification-body-image-width = 200;
+      };
+    };
+  };
+}
